f9cdceae6c35b9670b5440d07249d5c60b45687c
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-02 08:54:52 +0100

    Add script to find three numbers

--- day-1/index.js
+++ day-1/index.js
@@ -5,9 +5,11 @@ const entries = data.trim().split(/\r?\n/).map(i => +i);
 const sum  = 2020;
 for (let i = 0; i < entries.length; i++) {
   for (let j = i + 1; j < entries.length; j++) {
-    if (entries[i] + entries[j] === sum) {
-      console.log(entries[i], entries[j]);
-      console.log(entries[i] * entries[j]);
+    for (let k = j + 1; k < entries.length; k++ ) {
+      if (entries[i] + entries[j] + entries[k]=== sum) {
+        console.log(entries[i], entries[j], entries[k]);
+        console.log(entries[i] * entries[j] * entries[k]);
+      }
     }
   }
 }
